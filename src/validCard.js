ceccc04b45d8ddbd61081a11404c65320b691f68
imani.devs <imaniroberts.dev@gmail.com>
2023-04-18 17:40:58 -0400

    updated algo

--- src/validCard.js
+++ src/validCard.js
@@ -1,16 +1,21 @@
 const validCard = (cardNumber) => {
-let sum = 0;
-		const digits = cardNumber.toString();
-		for (let i = digits.length - 1; i >= 0; i--) { // start at the end of the string (right)
-			let digit = parseInt(digits[i]);
+	let sum = 0;
+	const digits = cardNumber.toString();
+	for (let i = digits.length - 1; i >= 0; i--) {
+		let digit = parseInt(digits[i]);
+		if ((digits.length - i) % 2 === 0) {
 			digit *= 2;
-			sum += digit;
-			if (sum % 10 === 0) {
-				return 'Valid';
-			} else {
-				return 'Not a valid card number';
+			if (digit > 9) {
+				digit -= 9;
 			}
-        }
+		}
+		sum += digit;
 	}
+	if (sum % 10 === 0) {
+		return 'Valid';
+	} else {
+		return 'Not a valid card number';
+	}
+};
 
 export default validCard;
