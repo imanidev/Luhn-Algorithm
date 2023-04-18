edbcdd12ea279776be41430b8d14069e49ef05b2
imani.devs <imaniroberts.dev@gmail.com>
2023-04-18 17:42:27 -0400

    update <br>

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Form from './Form';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Credit Card Validation</h1>
-      <Form />
-      <br></br>
-      <button>Submit</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Credit Card Validation</h1>
+			<Form />
+			<br />
+			<button>Submit</button>
+		</div>
+	);
 };
 
 export default App;
